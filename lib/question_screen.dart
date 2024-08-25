6fbcccfcde98300554d121c4998b4a6d57fe932e
aakritibansal25 <aakritibansal7654@gmail.com>
2024-08-25 17:47:03 +0530

    Updated

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionScreen extends StatefulWidget{
   const QuestionScreen({super.key});
@@ -11,41 +12,57 @@ class QuestionScreen extends StatefulWidget{
 }
 
 class _QuestionScreenState extends State<QuestionScreen>{
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(){
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context){
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width:double.infinity ,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(
-            color: Colors.white,
-          ),),
-        const SizedBox(height: 30,),
-        ...currentQuestion.answer.map((answer) {
-          return AnswerButton(answerText: answer, onTap: () {});
-        }),
-        AnswerButton(
-          answerText: currentQuestion.answer[0], 
-          onTap: () {}
-        ),
-        AnswerButton(
-          answerText: currentQuestion.answer[1], 
-          onTap: () {}
-        ),
-        AnswerButton(
-          answerText: currentQuestion.answer[2], 
-          onTap: () {}
-        ),
-        AnswerButton(
-          answerText: currentQuestion.answer[3], 
-          onTap: () {}
-        ),
-      ],),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+          Text(
+            currentQuestion.text,
+            style: GoogleFonts.lato(
+              color:  Color.fromARGB(255, 239, 237, 243),
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30,),
+          ...currentQuestion.getShuffledAnswers().map((answer) {
+            return AnswerButton(answerText: answer, onTap:answerQuestion);
+          }),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[0], 
+          //   onTap: () {}
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[1], 
+          //   onTap: () {}
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[2], 
+          //   onTap: () {}
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[3], 
+          //   onTap: () {}
+          // ),
+        ],),
+      ),
     );
   }
 }
